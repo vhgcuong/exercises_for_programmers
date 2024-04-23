e4433e65b72016d94682d951b51b429167211153
Yotoko <vhgcuong95@gmail.com>
2024-04-23 22:18:11 +0700

    saying hello

--- chapter_2/saying_hello/src/main.rs
+++ chapter_2/saying_hello/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("What is your name? ");
+    io::stdout().flush().expect("Error...");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error input...");
+    println!("Hello, {}, nice to meet you!", input.trim());
 }
