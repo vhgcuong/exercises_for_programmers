95ec372a22e90d3d823d3b9cf56c89908fca0bea
Yotoko <vhgcuong95@gmail.com>
2024-05-03 15:24:37 +0700

    Refactor code

--- chapter_2/counting_the_number_of_characters/src/main.rs
+++ chapter_2/counting_the_number_of_characters/src/main.rs
@@ -5,13 +5,15 @@ fn main() {
     loop {
         print!("What is the input string? ");
         io::stdout().flush().expect("Error...");
+
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Error input...");
 
         let str_input = input.trim();
-        if str_input.chars().count() < 1 {
+
+        if str_input.is_empty() {
             println!("Must enter something into the program.");
         } else {
             println!("{} has {} characters", str_input, str_input.chars().count());
