6370f3591a2397ad886d86bbcaee35b6f20b1c69
Yotoko <vhgcuong95@gmail.com>
2024-05-04 23:31:05 +0700

    Refactor code Simple math #1

--- chapter_2/simple_math/src/main.rs
+++ chapter_2/simple_math/src/main.rs
@@ -1,65 +1,35 @@
 use std::io;
 use std::io::Write;
 
-fn logic(first: u32, second: u32) {
-    match first.checked_add(second) {
-        Some(_) => {
-            println!("{first} + {second} = {}", first + second);
-        },
-        None => {
-            println!("Error: {first} + {second} (u32:MAX = 4_294_967_295u32)");
-        }
-    }
-
-    match first.checked_sub(second) {
-        Some(_) => {
-            println!("{first} - {second} = {}", first - second);
-        },
-        None => {
-            println!("Error: {first} - {second} (u32:MIN = 0)");
-        }
-    }
-
-    match first.checked_mul(second) {
-        Some(_) => {
-            println!("{first} * {second} = {}", first * second);
-        },
-        None => {
-            println!("Error: {first} * {second} (u32:MAX = 4_294_967_295u32)");
-        }
-    }
-
-    match first.checked_div(second) {
-        Some(_) => {
-            println!("{first} / {second} = {}", first / second);
-        },
-        None => {
-            println!("Error: {first} / {second} (u32:MAX = 4_294_967_295u32)");
-        }
-    }
-}
-
-fn input_keyboard(text: String) -> u32 {
-    loop {
-        print!("{text} ");
-        io::stdout().flush().expect("Error...");
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Error...");
-
-        match input.trim().parse::<u32>() {
-            Ok(number) => return number,
-            Err(_) => {
-                println!("The value entered is not numeric. Please try again!");
-                continue
-            }
-        }
-    }
-}
-
 fn main() {
-    let first = input_keyboard(String::from("What is the first number?"));
-    let second = input_keyboard(String::from("What is the second number?"));
+    let get_num = |msg| loop {
+        print!("{msg}");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
 
-    logic(first, second);
+        io::stdin().read_line(&mut input).unwrap();
+        if let Ok(num) = input.trim().parse::<u32>() {
+            break num;
+        }
+
+        println!("Invalid input. Please enter a number.");
+    };
+
+    let first = get_num("What is the first number? ");
+    let second = get_num("What is the second number? ");
+
+    for op in ["+", "-", "*", "/"] {
+        let result = match op {
+            "+" => first.checked_add(second),
+            "-" => first.checked_sub(second),
+            "*" => first.checked_mul(second),
+            "/" => first.checked_div(second),
+            _ => unreachable!()
+        };
+
+        match result {
+            Some(value) => println!("{first} {op} {second} = {value}"),
+            None => println!("Error: Overflow for operation {first} {op} {second}")
+        }
+    }
 }
