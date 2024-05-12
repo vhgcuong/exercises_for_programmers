36c16153995a9c18ed51676aa44c022a115c1a6d
Yotoko <vhgcuong95@gmail.com>
2024-05-12 15:35:13 +0700

    Refactoring retirement_calculator

--- chapter_2/retirement_calculator/src/main.rs
+++ chapter_2/retirement_calculator/src/main.rs
@@ -1,37 +1,33 @@
-use std::io;
-use std::io::Write;
+use std::io::{self, Write};
 use chrono::{Local, Datelike};
 
 fn main() {
-
     loop {
-        let current_age = input_keyboard(String::from("What is your current age?"));
-        let retire = input_keyboard(String::from("At what age would you like to retire?"));
+        let current_age = input_keyboard("What is your current age?");
+        let retire = input_keyboard("At what age would you like to retire?");
 
         if retire < current_age {
-            println!("Current age < retire ===> Error...");
-            continue
+            println!("Current age is greater than retirement age. Please enter valid ages.");
+            continue;
         }
 
-        println!("You have {} years left until you can retire.", retire - current_age);
+        let years_left = retire - current_age;
+        println!("You have {} years left until you can retire.", years_left);
 
-        let local = Local::now();
-        let year = local.year();
-
-        println!("It's {year}, so you can retire in {}.", year + retire - current_age);
+        let current_year = Local::now().year();
+        let retirement_year = current_year + years_left;
+        println!("It's {}, so you can retire in {}.", current_year, retirement_year);
         break;
     }
-
 }
 
-
-fn input_keyboard(text: String) -> i32 {
-    return loop {
-        print!("{text} ");
+fn input_keyboard(prompt: &str) -> i32 {
+    loop {
+        print!("{} ", prompt);
         io::stdout().flush().unwrap();
         let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-        io::stdin().read_line(&mut input).unwrap();
         if let Ok(num) = input.trim().parse::<i32>() {
             break num;
         }
