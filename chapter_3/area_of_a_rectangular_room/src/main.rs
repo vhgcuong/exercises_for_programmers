4559e24bd326024e290bacf626c868329bcb8e38
Yotoko <vhgcuong95@gmail.com>
2024-05-14 22:48:42 +0700

    Generic Struct Rec #2

--- chapter_3/area_of_a_rectangular_room/src/main.rs
+++ chapter_3/area_of_a_rectangular_room/src/main.rs
@@ -2,6 +2,34 @@ use std::io;
 use std::io::Write;
 use std::num::ParseIntError;
 
+const CONSTANT_METER: f64 = 0.09290304;
+
+struct Rectangular<T> {
+    width: T,
+    length: T
+}
+
+impl<T> Rectangular<T>  {
+    fn square<T>(&self) -> T {
+        match self.length.checked_mul(self.width) {
+            None => panic!(""),
+            Some(_) => self.length * self.width
+        }
+    }
+
+    fn convert_to_meters(&self) -> f64 {
+        self.square() as f64 * CONSTANT_METER
+    }
+
+    fn get_width<T>(&self) -> &T {
+        self.width
+    }
+
+    fn get_length(&self) -> &T {
+        self.length
+    }
+}
+
 fn main() {
     let length = match input_terminal("What is the length of the room in feet?") {
         Ok(number) => number,
@@ -12,9 +40,13 @@ fn main() {
         Err(_) => panic!("Input validate")
     };
 
-    let square = length * width;
-    println!("You entered dimensions of {length} feet by {width} feet.");
-    println!("The area is {} square feet {:.3} square meters", length * width, square as f64 * 0.09290304);
+    let rec = Rectangular {
+        width,
+        length
+    };
+
+    println!("You entered dimensions of {} feet by {} feet.", rec.get_length(), rec.get_width());
+    println!("The area is {} square feet {:.3} square meters", rec.square(), rec.convert_to_meters());
 }
 
 fn input_terminal(text: &str) -> Result<u32, ParseIntError> {
