0d8effc94468f7ed6d01debac41c59878bdbdcf3
Yotoko <vhgcuong95@gmail.com>
2024-05-14 22:13:08 +0700

    Area of a rectangular room #2

--- chapter_3/area_of_a_rectangular_room/src/main.rs
+++ chapter_3/area_of_a_rectangular_room/src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::io::Write;
+use std::num::ParseIntError;
+
 fn main() {
-    println!("Hello, world!");
+    let length = match input_terminal("What is the length of the room in feet?") {
+        Ok(number) => number,
+        Err(_) => panic!("Input validate")
+    };
+    let width = match input_terminal("What is the width of the room in feet?") {
+        Ok(number) => number,
+        Err(_) => panic!("Input validate")
+    };
+
+    let square = length * width;
+    println!("You entered dimensions of {length} feet by {width} feet.");
+    println!("The area is {} square feet {:.3} square meters", length * width, square as f64 * 0.09290304);
+}
+
+fn input_terminal(text: &str) -> Result<u32, ParseIntError> {
+    print!("{text} ");
+    io::stdout().flush().expect("Error...");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error...");
+
+    input.trim().parse::<u32>()
 }
