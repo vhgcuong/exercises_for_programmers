56b2a324504046730f0f0a64508e3ef6908a91d8
Yotoko <vhgcuong95@gmail.com>
2024-05-21 22:29:33 +0700

    Struct

--- j4fun/src/main.rs
+++ j4fun/src/main.rs
@@ -1,3 +1,21 @@
+use std::env::consts::FAMILY;
+
+#[derive(Debug)]
+struct Configuration {
+    version: u32,
+    active: bool
+}
+
+impl Configuration {
+    fn default() -> Configuration {
+        Configuration {
+            version: 0,
+            active: false
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let default_config = Configuration::default();
+    println!("{:?}", default_config)
 }
