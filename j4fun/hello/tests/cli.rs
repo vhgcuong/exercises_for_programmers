233ec245c8ce72d90dd1af0229a4fd249ff07f33
Yotoko <vhgcuong95@gmail.com>
2024-05-27 22:24:29 +0700

    Writing and running integration tests

--- j4fun/hello/tests/cli.rs
+++ j4fun/hello/tests/cli.rs
@@ -8,5 +8,18 @@ fn works() {
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
+
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
+
